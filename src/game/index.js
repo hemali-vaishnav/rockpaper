43dbc291d5c41d8d725f7e24c2f52d26ad634f9d
hemali-vaishnav <hemalivaishnav11@gmail.com>
2024-06-19 11:39:24 +0530

    some changes

--- src/game/index.js
+++ src/game/index.js
@@ -1,113 +1,81 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import './game.css';
-
 import rockimage from '../assets/images/rock.png';
 import paperimage from '../assets/images/paper.png';
 import scissorsimage from '../assets/images/scissors.png';
 
-
-
-
-let userScore = 0;
-let compScore = 0;
-
-const choices = document.querySelectorAll(".choice");
-const msg = document.querySelector("#msg");
-
-const userScorePara = document.querySelector("#user-score");
-const compScorePara = document.querySelector("#comp-score");
-
-const genCompChoice = () => {
-    const options = ["rock", "paper", "scissors"];
-    const randIdx = Math.floor(Math.random() * 3);
-    return options[randIdx]; 
-};
-
-const drawGame = () => {
-    msg.innerText = "Game was Draw. Play again.";
-    msg.style.backgroundColor = "#081b31";
-};
-
-const showWinner = (userWin, userChoice, compChoice) => {
-    if (userWin) {
-        userScore++;
-        userScorePara.innerText = userScore;
-        msg.innerText = `You win! Your ${userChoice} beats ${compChoice}`;
-        msg.style.backgroundColor = "green";
-    } else {
-        compScore++;
-        compScorePara.innerText = compScore;
-        msg.innerText = `You lost. ${compChoice} beats your ${userChoice}`;
-        msg.style.backgroundColor = "red";
-    } 
-};
-
-
-const playGame = (userChoice) => {
-    //Generate computer choice
-    const compChoice = genCompChoice();
-
-    if (userChoice === compChoice) {
-        //Draw Game
-        drawGame();
-    } else {
-        let userWin = true;
-        if (userChoice === "rock") {
-            //scissors, paper
-            userWin = compChoice === "paper" ? false : true;
-        } else if (userChoice === "paper") {
-            //rock, scissors
-            userWin = compChoice === "scissors" ? false : true;
-        } else {
-            //rock, paper
-            userWin = compChoice === "rock" ? false : true;
-        }  
-        showWinner(userWin, userChoice, compChoice);
-    }
-};
-
-choices.forEach((choice) => {
-    choice.addEventListener("click", () => {
-        const userChoice = choice.getAttribute("id");
-        playGame(userChoice);
-    });
-});
-
-
-
 export default function Game() {
+    const [userScore, setUserScore] = useState(0);
+    const [compScore, setCompScore] = useState(0);
+    const [message, setMessage] = useState('Play your move');
+
+    const genCompChoice = () => {
+        const options = ["rock", "paper", "scissors"];
+        const randIdx = Math.floor(Math.random() * 3);
+        return options[randIdx]; 
+    };
+
+    const drawGame = () => {
+        setMessage("Game was Draw. Play again.");
+    };
+
+    const showWinner = (userWin, userChoice, compChoice) => {
+        if (userWin) {
+            setUserScore(prevScore => prevScore + 1);
+            setMessage(`You win! Your ${userChoice} beats ${compChoice}`);
+        } else {
+            setCompScore(prevScore => prevScore + 1);
+            setMessage(`You lost. ${compChoice} beats your ${userChoice}`);
+        }
+    };
+
+    const playGame = (userChoice) => {
+        const compChoice = genCompChoice();
+        if (userChoice === compChoice) {
+            drawGame();
+        } else {
+            let userWin = true;
+            if (userChoice === "rock") {
+                userWin = compChoice === "paper" ? false : true;
+            } else if (userChoice === "paper") {
+                userWin = compChoice === "scissors" ? false : true;
+            } else {
+                userWin = compChoice === "rock" ? false : true;
+            }
+            showWinner(userWin, userChoice, compChoice);
+        }
+    };
+
     return (
         <>
             <h1>Rock Paper Scissors</h1>
             <div className="choices">
-                <div className="choice" id="rock">
-                    <img src={rockimage} />
+                <div className="choice" id="rock" onClick={() => playGame('rock')}>
+                    <img src={rockimage} alt="rock" />
                 </div>
-                <div className="choice" id="paper">
-                    <img src={paperimage} />
+                <div className="choice" id="paper" onClick={() => playGame('paper')}>
+                    <img src={paperimage} alt="paper" />
                 </div>
-                <div className="choice" id="scissors">
-                    <img src={scissorsimage} />
+                <div className="choice" id="scissors" onClick={() => playGame('scissors')}>
+                    <img src={scissorsimage} alt="scissors" />
                 </div>
             </div>
 
             <div className="score-board">
                 <div className="score">
-                    <p id="user-score">0</p>
+                    <p id="user-score">{userScore}</p>
                     <p>You</p>
                 </div>
                 <span>Vs</span>
                 <div className="score">
-                    <p id="comp-score">0</p>
+                    <p id="comp-score">{compScore}</p>
                     <p>Computer</p>
                 </div>
             </div>
 
             <div className="msg-container">
-                <p id="msg">Play your move</p>
+                <p id="msg">{message}</p>
             </div>
-
         </>
-    )
+    );
 }
